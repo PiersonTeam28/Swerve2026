76be7148f6db43e570c558e39bedb1bd92cb8495
Mike Arnold <mikearnold514@gmail.com>
2026-01-11 08:07:06 -0500

    finished with revisions

--- src/main/java/frc/robot/subsystems/ElevatorUtil.java
+++ src/main/java/frc/robot/subsystems/ElevatorUtil.java
@@ -7,8 +7,6 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 
-import edu.wpi.first.wpilibj.CAN;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
